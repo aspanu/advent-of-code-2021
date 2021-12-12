939bc9b0c74dcc05d016719b723c7137379605a3
Adrian Spanu <adri@nspanu.ca>
2021-12-12 14:13:28 -0600

    Finishing half of Day 8 and making the files for Day9-12.

---
+++ src/main/kotlin/Day11.kt
@@ -0,0 +1,28 @@
+
+fun main() {
+    println(day11Part1(day11TestInput))
+    println(day11Part1(day11PuzzleInput))
+    println(day11Part2(day11TestInput))
+    println(day11Part2(day11PuzzleInput))
+}
+
+fun day11Part1(input: String): Int {
+    return 0
+}
+
+fun day11Part2(input: String): Int {
+    return 0
+}
+
+const val day11TestInput = """"""
+
+const val day11PuzzleInput = """2138862165
+2726378448
+3235172758
+6281242643
+4256223158
+1112268142
+1162836182
+1543525861
+1882656326
+8844263151"""
---
+++ src/main/kotlin/Day12.kt
@@ -0,0 +1,42 @@
+
+fun main() {
+    println(day12Part1(day12TestInput))
+    println(day12Part1(day12PuzzleInput))
+    println(day12Part2(day12TestInput))
+    println(day12Part2(day12PuzzleInput))
+}
+
+fun day12Part1(input: String): Int {
+    return 0
+}
+
+fun day12Part2(input: String): Int {
+    return 0
+}
+
+const val day12TestInput = """"""
+
+const val day12PuzzleInput = """by-TW
+start-TW
+fw-end
+QZ-end
+JH-by
+ka-start
+ka-by
+end-JH
+QZ-cv
+vg-TI
+by-fw
+QZ-by
+JH-ka
+JH-vg
+vg-fw
+TW-cv
+QZ-vg
+ka-TW
+ka-QZ
+JH-fw
+vg-hu
+cv-start
+by-cv
+ka-cv"""
